d46c7c947747837eb0e9f184cb4e636004a21ce8
V-koushik <koushikvedullapalli@gmail.com>
2026-02-28 03:20:22 +0530

    alloe universal cors for bakend api

--- backend/main.py
+++ backend/main.py
@@ -61,9 +61,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "https://*.vercel.app",
+        "*",
     ],
     allow_credentials=True,
     allow_methods=["*"],
